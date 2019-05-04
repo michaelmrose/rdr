069f1dc3c9f120447a70b9e44663a395bd1a2dbf
Michael Rose <michael@rosenetwork.net>
2019-05-04 10:10:01 -0700

    added required libraries

--- src/rdr/core.clj
+++ src/rdr/core.clj
@@ -1,4 +1,14 @@
 (ns rdr.core
+  (:require
+   [clojure.java.shell :as ex]
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [com.rpl.specter :as ?]
+   [swiss.arrows :refer :all]
+   [me.raynes.fs :as fs]
+   [clojure.data.json :as json]
+   [pandect.algo.adler32 :refer :all]
+   )
   (:gen-class))
 
 (defn -main
