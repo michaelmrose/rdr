defa73e64f3f028cd88a4a6e16833a8ea47b7a45
Michael Rose <michael@rosenetwork.net>
2019-05-24 01:27:47 -0700

    touch ups

--- src/rdr/core.clj
+++ src/rdr/core.clj
@@ -18,12 +18,15 @@
 
 (def help-text
   "Usage:
-  -q [query] => pass query with the same syntax as calibredb or calibregui accepts
-  -r         => filter the most recent 30 distinct books opened via rdr via rofi or dmenu
-  -l         => open the last book read
-  -o [file]  => open with zathura and record in recent reads if part of a calibre library
-
-  See proposed help text for a more complete cli to be implemented ")
+  -p [list]     => list of formats in order of preference eg pdf,epub,mobi
+  -S [options]  => save options to disk
+  -k [number]   => number of recent reads to keep
+  -q [query]    => pass query with the same syntax as calibredb or calibregui accepts
+  -r            => filter the most recent 30 distinct books opened via rdr via rofi or dmenu
+  -l            => open the last book read
+  -o [file]     => open with default reader and record in recent reads if part of a calibre library
+  afile.pdf     => same as -o afile.pdf
+  a query here  => same as -q a query here")
 
 (def proposed-help-text
   "Usage:
@@ -83,11 +86,9 @@
    determine what reader command to use. map could be passed quoted at cli or a single application name
    passed with -r ex: -r zathura or -r '{epub somereader pdf zathura else foobar}'This could ultimately be part of a configuration file"
   [book]
-  (let [ext (fs/extension (calibre/select-preferred-ebook-format book (:preferred opts)))])
-  "xdg-open"
-  )
-
-(return-ebook-reader-command {:authors "Amit Rathore", :formats ["/home/michael/books/Amit Rathore/Clojure in Action (3)/Clojure in Action - Amit Rathore.epub" "/home/michael/books/Amit Rathore/Clojure in Action (3)/Clojure in Action - Amit Rathore.pdf"], :id 3, :title "Clojure in Action"} )
+  ;; (let [ext (fs/extension (calibre/select-preferred-ebook-format book (:preferred opts)))]
+  ;;   ext)
+  "xdg-open")
 
 (defn save-book-to-recent-reads 
   "saves book info map to ~/.config/booksclj/recent.edn keeping only the most recent n entries 
@@ -95,7 +96,7 @@
   [book]
   (let [recent (get-configuration-file-path "recent")
         current (string/split-lines(slurp recent))
-        combined (string/join "\n"(take (:number opts) (distinct(flatten [(str book) current]))))]
+        combined (string/join "\n"(take (:keep opts) (distinct(flatten [(str book) current]))))]
     (spit recent combined)) (println "saving to recent list"))
 
 ;;TODO this opens the file with xdg-open open rather than depending on an expressed preference in reader
@@ -132,7 +133,6 @@
   (let [recent (get-configuration-file-path "recent")]
     (map read-string (string/split-lines(slurp recent)))))
 
-
 (defn open-last-book 
   "Open most recent entry from recent reads"
   []
@@ -162,14 +162,16 @@
    ["-p" "--preferred FORMATS"
     :parse-fn #(string/split % #",")]
    ["-S" "--save"]
-   ["-R" "--reader"]])
+   ;; TODO: Impliment this properly
+   ;; ["-R" "--reader DEFAULTS"
+   ;;  :parse-fn read-string ]
+   ])
 
 (def default-options
   {:keep 30
-   :preferred [".epub" ".pdf"]}
+   :preferred [".pdf" ".epub" ".mobi"]
+   :reader {:default "xdg-open"}}
   )
-
---reader .pdf=zathura
 
 (defn -main
   "Parse arguments and decide what action to take."
@@ -184,16 +186,13 @@
     (:recent opts) (pick-from-recent-reads)
     (:last opts) (open-last-book)
     (:open opts) (open-ebook-file arguments)
-    (:query opts) (query-and-open arguments))
+    (:query opts) (query-and-open arguments)
+    :else (if (fs/exists? arguments) 
+            (open-ebook-file arguments)
+            (query-and-open arguments)
+            ))
 
   ;; process takes several seconds to properly terminate if we don't exit manually
   ;; yet obviously we don't want to kill the repl every time we run test main
   (if-not (is-in-repl?) 
     (shutdown-agents)))
-
-;; (-main "-l" "-p" ".pdf,.epub"  "clojure" "action")
-;; (-main "-r" "-p" ".pdf,.epub"  "clojure" "action")
-(-main "-p" ".pdf,.epub" "-q" "clojure")
-;; (-main "-k" "20" "-o" "halo.pdf")
-;; (-main "-S" "-k" "32" "-p" ".pdf,.epub")
-
