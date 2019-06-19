d7b12a236d8607039dae9842106e36457c762401
Michael Rose <michael@rosenetwork.net>
2019-06-19 12:15:48 -0700

    simplified and fixed an error where it wouldn't add the first recent entry introduced by fixing ANOTHER error

--- src/rdr/core.clj
+++ src/rdr/core.clj
@@ -31,8 +31,7 @@
    where n is either 30 or the value defined by --keep"
   [book]
   (if-let* [recent-config-file (get-configuration-file-path "recent")
-            current (map read-string (string/split-lines (slurp recent-config-file)))
-            combined (take (:keep opts) (distinct-by #(select-keys % [:id :library])  (flatten [book current])))]
+            combined (take (:keep opts) (distinct-by #(select-keys % [:id :library])  (flatten [book (list-recent-reads!)])))]
            (spit recent-config-file (string/join "\n" combined))
            (println "saving to recent list")))
 
