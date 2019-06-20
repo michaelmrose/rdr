453f0098e03ac043702c9c804b5b303dcf927470
Michael Rose <michael@rosenetwork.net>
2019-06-19 17:37:21 -0700

    formatting on help text improved

--- src/rdr/core.clj
+++ src/rdr/core.clj
@@ -13,61 +13,62 @@
 (declare opts)
 
 (def help-text
-  "
-    # rdr
+  "• rdr
 
     A handy cli interface to searching and opening books from your calibre library or recently read titles from your library.
 
-    ## Installation
+    • Installation
 
-    Download from releases. Building requires oracles new AOT compiler for jvm based languages [graal](http://www.graalvm.org) Be advised that it is extremely memory hungry.
+        Download from releases. Building requires oracles new AOT compiler for jvm based languages [graal](http://www.graalvm.org) Be advised that it is extremely memory hungry.
 
-    ## Usage
+    • Usage
 
-    ### Actions
+        • Actions
 
-    -q [query]     => pass query with the same syntax as calibredb or calibregui accepts
+            -q [query]     => pass query with the same syntax as calibredb or calibregui accepts
 
-    -l             => open the last book read
+            -l             => open the last book read
 
-    -r             => filter the most recent 30 distinct books opened via rdr via rofi or dmenu
+            -r             => filter the most recent 30 distinct books opened via rdr via rofi or dmenu
 
-    a query string => same as -q a query string
+            a query string => same as -q a query string
 
-    -o [file]      => open with default reader and record in recent reads if part of a calibre library
+            -o [file]      => open with default reader and record in recent reads if part of a calibre library
 
-    ### Options
+        • Options
 
-    -p [list]      => list of formats in order of preference eg pdf,epub,mobi
+            -p [list]      => list of formats in order of preference eg pdf,epub,mobi
 
-    -k [number]    => number of recent reads to keep
+            -k [number]    => number of recent reads to keep
 
-    --port PORT
+            --port PORT
 
-    --server URL
+            --server URL
 
-    --user USER
+            --user USER
 
-    --password PASSWORD
+            --password PASSWORD
 
-    -S [options]   => save options passed to disk
+            -S [options]   => save options passed to disk
 
-    If you don't want to keep passing the above options consider running rdr -S option1 value option2 value to save said options then you may omit them from future invocations.
+            If you don't want to keep passing the above options consider running rdr -S option1 value option2 value to save said options then you may omit them from future invocations.
 
-    ## Please Note
+    • Please Note
 
     Please note that calibres content server must be running for this program to work while calibre is running as it must communicate with the
     content server process instead of directly using calibredb to examine the database. If neccesary please specify the server,port,username,and password.
     The default is http://localhost:8080 with no password
 
-    ## Example
+    • Example
 
-    [example video](https://www.youtube.com/watch?v=RuWe0uhzrXE&)
+    example video https://www.youtube.com/watch?v=RuWe0uhzrXE&
 
-    ### Limitations
-    Rdr doesn't actually support remote calibre servers yet as it presently just uses this feature to work around the fact that calibredb wont work locally without talking to the calibre content server.  It could probably be trivially expanded to fetch and then display remote books in the future.
+    • Limitations
 
-    ## License
+    Rdr doesn't actually support remote calibre servers yet as it presently just uses this feature to work around the fact that calibredb wont work locally without talking to the
+    calibre content server.  It could probably be trivially expanded to fetch and then display remote books in the future.
+
+    • License
 
     GPL 3.0 or later.
     Copyright © 2019 Michael Rose
